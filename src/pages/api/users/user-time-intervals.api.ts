296d4ff2dc7aca8e99ce32167cdf80565db57587
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-04-22 17:03:46 -0300

    saving the time interval on DB

--- src/pages/api/users/user-time-intervals.api.ts
+++ src/pages/api/users/user-time-intervals.api.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const timeIntervalsFormSchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,7 +24,24 @@ export default async function handler(
 
   const session = await getServerSession(req, res, authOptions(req, res))
 
-  return res.json({
-    session,
-  })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsFormSchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeIntervals.create({
+        data: {
+          week_day: interval.weekDay,
+          start_time_in_minutes: interval.startTimeInMinutes,
+          end_time_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
