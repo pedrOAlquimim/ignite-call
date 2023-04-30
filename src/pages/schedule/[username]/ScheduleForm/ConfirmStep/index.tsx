fd43310d881a6a57830624a9a81d97742c08a6b1
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-04-30 18:13:30 -0300

    Confirm step styles done

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,44 @@
+import { Button, Text, TextArea, TextInput } from '@pedro-ignite-ds/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+export function ConfirmStep() {
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,5 @@
-import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep></CalendarStep>
+  return <ConfirmStep></ConfirmStep>
 }
