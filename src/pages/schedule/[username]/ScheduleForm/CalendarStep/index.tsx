2f38df3951fbca6714ce297d619e923db3f696b5
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-04-30 17:57:39 -0300

    TimePicker styles done

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+            <TimePickerItem>13:00</TimePickerItem>
+            <TimePickerItem>14:00</TimePickerItem>
+            <TimePickerItem>15:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>17:00</TimePickerItem>
+            <TimePickerItem>18:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
