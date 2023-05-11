55a3560cbca87e103a2ab59565668f73dc616e3f
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-05-10 22:48:35 -0300

    available times route

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date does not provided' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User do not found' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeIntervals.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ availability: [] })
+  }
+
+  const { start_time_in_minutes, end_time_in_minutes } = userAvailability
+
+  const startHour = start_time_in_minutes / 60
+  const endHour = end_time_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  return res.json(possibleTimes)
+}
