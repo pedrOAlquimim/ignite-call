22627e0341b14971f2669ddf99e940a795b592d7
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-05-18 22:21:40 -0300

    Migrating db to MySql with Docker

--- prisma/migrations/20230420021228_create_time_intervals_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "user_time_intervals" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "week_day" INTEGER NOT NULL,
-    "start_time_in_minutes" INTEGER NOT NULL,
-    "end_time_in_minutes" INTEGER NOT NULL,
-    "user_id" TEXT NOT NULL,
-    CONSTRAINT "user_time_intervals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
